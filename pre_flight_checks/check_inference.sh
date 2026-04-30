bcfb2ba16a4b5c8d75ec4deaab2bbb1eaf138673
Faisal Memon <faisalmemonuk@yahoo.com>
2026-04-30 21:20:14 +0100

    Snapshot my configuration files.  I am trying to use a nvidia pytorch base and source compiles on top.

--- pre_flight_checks/check_inference.sh
+++ pre_flight_checks/check_inference.sh
@@ -3,16 +3,10 @@
 # Define absolute paths for the container to make the script readable
 CONTAINER_DATA="/home/appuser/DiffDock/data"
 CONTAINER_CACHE="/home/appuser/.cache"
-
-# Ensure the local cache directory exists on your Spark host
-mkdir -p $(pwd)/../cache/torch
+INNER_COMMAND="pip install --force-reinstall --no-cache-dir --no-binary :all: torch-scatter torch-sparse torch-cluster torch-spline-conv && python inference.py --protein_path $CONTAINER_DATA/1a0q/1a0q_protein_processed.pdb --ligand_description $CONTAINER_DATA/1a0q/1a0q_ligand.sdf --out_dir $CONTAINER_DATA/1a0q/output --samples_per_complex 10"
 
 docker run --rm -it --gpus all \
   -v $(pwd)/../data:$CONTAINER_DATA \
   -v $(pwd)/../cache/torch:$CONTAINER_CACHE/torch \
   diffdock:gb10 \
-  micromamba run -n diffdock python inference.py \
-  --protein_path $CONTAINER_DATA/1a0q/1a0q_protein_processed.pdb \
-  --ligand_description $CONTAINER_DATA/1a0q/1a0q_ligand.sdf \
-  --out_dir $CONTAINER_DATA/1a0q/output \
-  --samples_per_complex 10
+  micromamba run -n diffdock /bin/bash -c "$INNER_COMMAND"
