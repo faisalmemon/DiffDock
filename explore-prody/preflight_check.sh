b1db1ff59dc2c48581c62b6e8c24314f573cd04d
Faisal Memon <faisalmemonuk@yahoo.com>
2026-05-02 12:25:03 +0100

    Add more support for preflight checks.

---
+++ explore-prody/preflight_check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+docker run --rm --gpus all \
+  -e NVIDIA_DISABLE_REQUIRE=true \
+  -e NVIDIA_VISIBLE_DEVICES=all \
+  explore_prody:gb10 \
+  python -c "
+  
+import torch;
+import torch_scatter;
+import torch_cluster;
+import prody;
+print(f'CUDA Available: {torch.cuda.is_available()}');
+print(f'GPU Device: {torch.cuda.get_device_name(0)}');
+print('Success: All libraries loaded and linked!');
+"
