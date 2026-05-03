6b27aa4f9b9f0faddcff0c31e47ec8d7e66dfb60
Faisal Memon <faisalmemonuk@yahoo.com>
2026-05-03 12:31:56 +0100

    Improve permission handling.

---
+++ docker_tag_push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker tag diffdock:gb10 ghcr.io/faisalmemon/diffdock/diffdock-app:gb10-v1
+docker push ghcr.io/faisalmemon/diffdock/diffdock-app:gb10-v1
--- pre_flight_checks/check_inference.sh
+++ pre_flight_checks/check_inference.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Define absolute paths for the container to make the script readable
-CONTAINER_DATA="/home/appuser/DiffDock/data"
-CONTAINER_CACHE="/home/appuser/.cache"
-INNER_COMMAND="pip install --force-reinstall --no-cache-dir --no-binary :all: torch-scatter torch-sparse torch-cluster torch-spline-conv && python inference.py --protein_path $CONTAINER_DATA/1a0q/1a0q_protein_processed.pdb --ligand_description $CONTAINER_DATA/1a0q/1a0q_ligand.sdf --out_dir $CONTAINER_DATA/1a0q/output --samples_per_complex 10"
-
-docker run --rm -it --gpus all \
-  -v $(pwd)/../data:$CONTAINER_DATA \
-  -v $(pwd)/../cache/torch:$CONTAINER_CACHE/torch \
+docker run --rm --gpus all \
+  --ipc=host \
+  --ulimit memlock=-1 \
+  --ulimit stack=67108864 \
   diffdock:gb10 \
-  micromamba run -n diffdock /bin/bash -c "$INNER_COMMAND"
+  python -m inference --config default_inference_args.yaml \
+  --protein_path data/sample_data/6o0i_protein.pdb \
+  --ligand_description "CC1=CC2=C(C=C1C)N(C=N2)C3C(C(C(O3)CO)O)O" \
+  --out_dir results/smoke_test
+
