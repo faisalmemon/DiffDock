e050a491ea03b2eb61678aceda4f4dcf1d9833f2
Faisal Memon <faisalmemonuk@yahoo.com>
2026-05-21 22:36:07 +0100

    Allow python files in datasets in the image.

--- run_diffdock.sh
+++ run_diffdock.sh
@@ -3,4 +3,5 @@
 docker run --gpus all --ipc=host \
   -v /home/faisalm/.cache/torch:/home/ubuntu/.cache/torch \
   -v /home/faisalm/dev/DiffDock/results:/home/ubuntu/DiffDock/results \
+  -v /home/faisalm/my_experimental_data:/home/ubuntu/DiffDock/data \
   ghcr.io/faisalmemon/diffdock/diffdock-app:gb10-v2
